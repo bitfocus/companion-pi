b62d55885241b1d128f1e7e694956338026acbb3
Julian Waller <git@julusian.co.uk>
2023-01-26 20:54:28 +0000

    fix 2.4

--- launch.sh
+++ launch.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# run it!
-/opt/companion/node-runtime/bin/node /opt/companion/main.js
+if [ -d /usr/local/src/companion ]; then
+    cd /usr/local/src/companion
+
+    if [ -f "UPDATE_IN_PROGRESS" ]; then
+        echo "Companion can not be started, an update operation was interrupted."
+        exit 5
+    fi
+
+    # run it!
+    /opt/fnm/aliases/default/bin/node headless_ip.js 0.0.0.0
+else
+    # run it!
+    /opt/companion/node-runtime/bin/node /opt/companion/main.js
+fi
