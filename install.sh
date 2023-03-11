e93a3f29f6d38d6cd48216538f14167f3c86a331
Julian Waller <git@julusian.co.uk>
2023-03-11 19:23:31 +0000

    wip

--- install.sh
+++ install.sh
@@ -5,17 +5,20 @@ echo "This will attempt to install Companion as a system service on this device.
 echo "It is designed to be run on headless servers, but can be used on desktop machines if you are happy to not have the tray icon."
 echo "A user called 'companion' will be created to run the service, and various scripts will be installed to manage the service"
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+if [ $(/usr/bin/id -u) -ne 0 ]; then
     echo "Must be run as root"
     exit 1
 fi
+
+COMPANIONPI_BRANCH="${COMPANIONPI_BRANCH:-main}"
+COMPANION_BRANCH="${COMPANION_BRANCH:-beta}"
 
 # add a system user
 adduser --disabled-password companion --gecos ""
 
 # install some dependencies
 apt-get update
-apt-get install -y git unzip curl libusb-1.0-0-dev libudev-dev cmake
+apt-get install -y git unzip curl libusb-1.0-0-dev libudev-dev
 apt-get clean
 
 # install fnm to manage node version
@@ -27,14 +30,14 @@ export PATH=/opt/fnm:$PATH
 eval "`fnm env --shell bash`",
 
 # clone the companionpi repository
-git clone https://github.com/bitfocus/companion-pi.git -b ${var.pibranch} /usr/local/src/companionpi
+git clone https://github.com/bitfocus/companion-pi.git -b $COMPANIONPI_BRANCH /usr/local/src/companionpi
 cd /usr/local/src/companionpi
 
 # configure git for future updates
 git config --global pull.rebase false
 
 # run the update script
-./update.sh ${var.branch}
+./update.sh $COMPANION_BRANCH
 
 # install update script dependencies, as they were ignored
 yarn --cwd "/usr/local/src/companionpi/update-prompt" install
@@ -46,3 +49,6 @@ systemctl enable companion
 # add the fnm node to this users path
 # TODO - verify permissions
 echo "export PATH=/opt/fnm/aliases/default/bin:\$PATH" >> /home/companion/.bashrc
+
+echo "Companion is installed!"
+echo "You can start it with `sudo systemctl start companion` or `sudo companion-update`"
